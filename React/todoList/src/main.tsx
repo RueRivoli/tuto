a2d0c8875d0b9dca44705d4b5ac93a8151202d68
florent gallois <flgallois@gmail.com>
2018-03-09 20:13:22 +0100

    tuto

--- React/todoList/src/main.tsx
+++ React/todoList/src/main.tsx
@@ -1,1 +1,6 @@
-document.body.innerHTML = 'Salut les gens'
+import {render} from 'react-dom'
+import * as React from 'react'
+render(
+    <div>Salut les gens</div>,
+    document.getElementById('app')
+)
