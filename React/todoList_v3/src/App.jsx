45d3a2860eee973ed2957b30e74b11baa0ea102e
Florent <flgallois@gmail.com>
2018-03-14 01:41:17 +0100

    todolist

---
+++ React/todoList_v3/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Header from './Components/header.jsx';
+import './App.css';
+
+class App extends Component {
+
+   constructor(props){
+       super(props);
+       this.state = {
+           todos:[
+               {id: 0, text: "Trouver un stage"},
+               {id: 1, text: "Faire un régime"},
+               {id: 2, text: "Voir les copains"}
+           ],
+           nextId: 3
+       };
+       this.addTodo = this.addTodo.bind(this);
+       this.removeTodo = this.removeTodo.bind(this);
+   }
+
+   addTodo(newText){
+       let todos = this.state.todos.slice();
+       todos.push({id: this.state.nextId, text: newText});
+       this.setState({
+            todos: todos,
+            nextId: ++this.state.nextId
+       });
+   }
+
+   removeTodo(id){
+        this.setState({
+            todos: this.state.todos.filter((todo, index) => todo.id !== id)
+        });
+   }
+
+
+  render() {
+    return (
+        <div className="App">
+            <div className="box">
+            <Header/>   
+            </div>
+        </div>
+  );
+  }
+}
+
+export default App;
