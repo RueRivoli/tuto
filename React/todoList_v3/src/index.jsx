5019889021882425bbe7773a95596bd53b84853c
Florent <flgallois@gmail.com>
2018-03-17 02:40:26 +0100

    enlever les node-modules

--- React/todoList_v3/src/index.jsx
+++ React/todoList_v3/src/index.jsx
@@ -2,7 +2,5 @@ import React from 'react';
 import {render} from 'react-dom';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
